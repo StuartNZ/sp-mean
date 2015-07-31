597e1f9f85c9e2a001eaa538872d29ad766ab0a7
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-07-31 09:31:09 +0200

    query all

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    monk = require('monk');
 
-app.get('/', function(req, res) {
+var swChars = monk('localhost:27017/starwars').get('character');
+
+app.get('/', function (req, res) {
     res.send("Hello from Express");
 });
 
-app.listen(3000, function() {
+app.get('/character', function (req, res) {
+    swChars.find({}, function (err, docs) {
+        if (err == null) {
+            res.json(docs);
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.listen(3000, function () {
     console.log("Server ready. Listening on port 3000");
 });
